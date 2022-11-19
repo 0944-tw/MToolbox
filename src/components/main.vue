ed5ed5d1ae0e82f9a21070a0feccc998267e978f
Loading.. Please Wait <69704016+ImLoadingUuU@users.noreply.github.com>
2022-11-20 07:58:02 +0800

    Update main.vue

--- src/components/main.vue
+++ src/components/main.vue
@@ -4,23 +4,25 @@
     <v-app>
       <v-app-bar color="teal">
         <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>MeTooIDK <b>Tools</b></v-toolbar-title>
+        <v-toolbar-title>MeTooIDK <b>{{ title }}</b></v-toolbar-title>
         <v-btn @click="toggleTheme">Theme</v-btn>
       </v-app-bar>
       <v-navigation-drawer v-model="drawer">
         <v-list density="compact" v-model="currentPage" nav>
-          <v-list-item prepend-icon="mdi-folder" @click="currentPage == 'home' ? currentPage = 'home' : currentPage = 'home';" title="Homes" value="home"></v-list-item>
-          <v-list-item prepend-icon="mdi-equalizer" @click="currentPage == 'apm' ? currentPage = 'home' : currentPage = 'apm';" title="APM Music" value="apm"></v-list-item>
-
+          <v-list-item prepend-icon="mdi-folder" @click="currentPage == 'home' ? ((currentPage = 'home' ),(title = 'Tools')): ((currentPage = 'home'),(title = 'Tools'));" title="Homes" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-equalizer" @click="currentPage == 'apm' ? ((currentPage = 'home'),(title = 'Tools')) : ((currentPage = 'apm',(title = 'MusicLibrary')));" title="APM Music" value="apm"></v-list-item>
+          <v-list-item prepend-icon="mdi-message" @click="currentPage == 'message' ? (( currentPage = 'home' ) , ( title = 'Tools')) : ((currentPage = 'message'),(title = 'ChatBeta'));" title="Chat Bot" value="cb"></v-list-item>
         </v-list>
       </v-navigation-drawer>
 
       <v-main>
         <v-container v-if="currentPage == 'home'">
           <v-card title="Welcome to MeTooIDK Tools" text="Because im lazy to download music so i created this.">
+
           </v-card>
+
         </v-container>
-        <v-container v-if="currentPage == 'apm'">
+        <v-container  v-if="currentPage == 'apm'">
           <apm></apm>
         </v-container>
       </v-main>
@@ -37,7 +39,8 @@ export default {
   data(){
     return {
       drawer: true,
-      currentPage: "home"
+      currentPage: "home",
+      title: "Tools"
     }
   },
   components: {
